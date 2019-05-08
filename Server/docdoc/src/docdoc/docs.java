b5e208024dd263dfe4567812fc5178265e789a4d
moonyeol <ansduf05@gmail.com>
2019-05-08 13:52:06 +0900

    doc기능

---
+++ Server/docdoc/src/docdoc/docs.java
@@ -0,0 +1,75 @@
+package docdoc;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+
+
+
+public class docs {
+	public void mkdoc(ArrayList<String> content,String path, String fname) throws FileNotFoundException, IOException, InvalidFormatException {
+
+		XWPFDocument document = new XWPFDocument();
+		XWPFParagraph tmpParagraph = document.createParagraph();
+		XWPFRun tmpRun = tmpParagraph.createRun();
+		
+		String tmp = "";
+		ListIterator<String> iterator = content.listIterator();
+		tmpRun.setFontSize(12);
+		
+		while(iterator.hasNext()) {
+			tmpRun.setText(iterator.next());
+			tmpRun.addBreak();
+		}
+
+		File dir = new File(path);
+
+		if (!dir.exists()) {//상위 디렉토리가 존재하지 않을 경우 생성
+			dir.mkdirs();
+		}
+
+		File saveFile = new File(path +"/"+ fname +"/seokee.doc");
+
+		FileOutputStream fos = new FileOutputStream(saveFile);
+		document.write(fos);
+		fos.close();
+	}
+	
+	public void mktxt(ArrayList<String> content,String path, String fname) {
+        try{
+		File dir = new File(path);
+		File saveFile = new File(path +"/"+ fname +".txt");
+		
+		if (!dir.exists()) {//상위 디렉토리가 존재하지 않을 경우 생성
+			dir.mkdirs();
+		}
+		BufferedWriter fw = new BufferedWriter(new FileWriter(saveFile, true));
+
+		ListIterator<String> iterator = content.listIterator();
+		
+		while(iterator.hasNext()) {
+			fw.write(iterator.next());
+			fw.newLine();
+		}
+		
+
+		fw.flush();
+		fw.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+	}
+	
+	
+
+}
