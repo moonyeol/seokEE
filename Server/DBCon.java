120f1c31aafb25231ab0c7fe078b939222ce0624
wool0826 <chanwool94@gmail.com>
2019-05-20 17:12:58 +0900

    server update

--- Server/DBCon.java
+++ Server/DBCon.java
@@ -1,4 +1,3 @@
-package seokee;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,31 +11,25 @@ import java.util.List;
 import java.util.Map;
 import java.sql.PreparedStatement;
 
-import kr.co.shineware.*;
 import kr.co.shineware.util.common.model.Pair;
-import kr.co.shineware.nlp.komoran.*;
 import kr.co.shineware.nlp.komoran.core.analyzer.Komoran;
 import java.sql.ResultSet;
 
-//jdbc:mysql://localhost/dbname?user=(ª˝∑´)&password=(ª˝∑´)&Unicode=true&characterEncoding=UTF-
-//String dbPath = String.format(
-//        "jdbc:mysql://%s:%d/%s?user=%s&password=%s&characterEncoding=utf-8&" + 
-//        "useUnicode=true", ci.host, ci.port, ci.dbName, ci.user, ci.password);
 public class DBCon {
 	static final String JDBC_DRIVER ="com.mysql.cj.jdbc.Driver";
     static final String url = "jdbc:mysql://seokee0503.c9p1xpsot2og.ap-northeast-2.rds.amazonaws.com/seokee";
-    static String USERNAME = "IndexOutOfRange";
+    static String USERNAME = "indexoutofrange";
     static String PASSWORD = "12341234";
     //static String ENCODEING = "Unicode=true&characterEncoding=UTF-8";
     public static Connection conn = null;
     public DBCon(){
     	try
         {
-            System.out.println("¡¢º”¡ﬂ¿‘¥œ¥Ÿ...");
+            System.out.println("Connecting...");
             Class.forName(JDBC_DRIVER);
             //conn = DriverManager.getConnection(url, USERNAME, PASSWORD);
-            conn = DriverManager.getConnection( "jdbc:mysql://seokee0517.c9p1xpsot2og.ap-northeast-2.rds.amazonaws.com/Seokee"
-            		+ "?user=IndexOutOfRange&password=12341234&characterEncoding=utf-8&"
+            conn = DriverManager.getConnection( "jdbc:mysql://seokee0503.c9p1xpsot2og.ap-northeast-2.rds.amazonaws.com/seokee"
+            		+ "?user=indexoutofrange&password=12341234&characterEncoding=utf-8&" 
             		+ "useUnicode=true");
             if(conn !=null ) System.out.println("DB Conncted.");
             else System.out.println("DB connect fail.");
@@ -49,7 +42,6 @@ public class DBCon {
                 }
     	
     }
-    // Talk «— ø≠¿ª DB¿« talk≈◊¿Ã∫Ì∑Œ ª¿‘
     public void insertTalk(Talk a) {
     	String query = "insert into talk (room,time,msg,id) values(?,?,?,?);";
     	PreparedStatement pstmt = null;
@@ -60,13 +52,13 @@ public class DBCon {
     		pstmt.setString(3, a.msg);
     		pstmt.setString(4, a.id);
     		pstmt.executeUpdate();
-    		System.out.println("Insert success");
+
+    		//System.out.println("Insert success");
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
     	}
     }
-    // String room ¿ª ∏≈∞≥∫Øºˆ∑Œ room¿« ∏µÁ ø≠¿ª ArrayList∑Œ π›»Ø
     public ArrayList<Talk> serachMessageRoom(String room) {
     	ArrayList<Talk> data = new ArrayList<>();
     	String query = "select * from talk where room = ? order by indexnum";
@@ -80,7 +72,7 @@ public class DBCon {
     		{
     			data.add(new Talk(rs.getString("room"), rs.getString("time"), rs.getString("msg"), rs.getString("id")));
     		}
-    		System.out.println("Calling success");
+    		//System.out.println("Calling success");
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
@@ -88,7 +80,6 @@ public class DBCon {
     	return data;
     	
     }
-    // String id ∏¶ ∏≈∞≥∫Øºˆ∑Œ room¿« ∏µÁ ø≠¿ª ArrayList∑Œ π›»Ø
     public ArrayList<Talk> serachMessageID(String id) {
     	ArrayList<Talk> data = new ArrayList<>();
     	String query = "select * from talk where id = ? order by indexnum";
@@ -101,14 +92,13 @@ public class DBCon {
     		{
     			data.add(new Talk(rs.getString("room"), rs.getString("time"), rs.getString("msg"), rs.getString("id")));
     		}
-    		System.out.println("Calling success");
+    		//System.out.println("Calling success");
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
     	}
     	return data;
     }
-    // String id ∞° ¡∏¿Á«œ¥¬¡ˆ boolean ∏Æ≈œ
     public boolean memberIDCheck(String id) {
     	String query = "select count(*) from member where id = ?";
     	PreparedStatement pstmt = null;
@@ -116,11 +106,13 @@ public class DBCon {
     		pstmt = conn.prepareStatement(query);
     		pstmt.setString(1, id);
     		ResultSet rs = pstmt.executeQuery();
-    		
-    		if (rs.getInt("count(*)")!=0) {
-    			System.out.println("Already existing ID");
-    			return false;
-    		}
+
+    		if(rs.next()){
+				if (rs.getInt("count(*)")!=0) {
+					System.out.println("[memberIDCheck] Already existing ID");
+					return false;
+				}
+			}
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
@@ -128,7 +120,6 @@ public class DBCon {
     	}
     	return true;
     }
-    // Member (id,password,gender,birth, nickname) ∏¶ DBø° ª¿‘, º∫∞¯«œ∏È true Ω«∆–«œ∏È false
     public boolean memberResgisterID(Member a) {
     	if (memberIDCheck(a.getID())) {
     		String query = "insert into member (id,password,gender,birth, nickname) values(?,?,?,?,?);";
@@ -141,7 +132,7 @@ public class DBCon {
         		pstmt.setString(4, a.getBirth());
         		pstmt.setString(5, a.getNickname());
         		pstmt.executeUpdate();
-        		System.out.println("Insert Member success");
+        		//System.out.println("Insert Member success");
         	}catch(SQLException e)
         	{
         		e.printStackTrace();
@@ -149,15 +140,13 @@ public class DBCon {
         	}
     	}
     	else {
-    		System.out.println("Register Fail, Already existing ID.");
+    		System.out.println("[memberRegisterID] Register Fail, Already existing ID.");
     		return false;
     	}
     	
-    	System.out.println("Register Complete.");
+    	System.out.println("[memberRegisterID] Register Complete.");
     	return true;
     }
-    
-    // id, password∏¶ ∏≈∞≥∫Øºˆ∑Œ idøÕ password∞° ∏¬¥¬¡ˆ »Æ¿Œ, true false π›»Ø
     public boolean memberLoginCheck(String id, String password) {
     	String query = "select password from member where id = ?";
     	PreparedStatement pstmt = null;
@@ -165,23 +154,23 @@ public class DBCon {
     		pstmt = conn.prepareStatement(query);
     		pstmt.setString(1, id);
     		ResultSet rs = pstmt.executeQuery();
-    		rs.next();
-    		String x; // DBø°º≠ password∏¶ πﬁæ∆ø¿¥¬ ∫Øºˆ
-    		x = rs.getString("password");
-    		if (x.contentEquals(password)) {
-    			System.out.println("id equals password");
-    			return true;
-    		}
-    		System.out.println("Calling success");
+    		
+			if(rs.next()){
+				String x; 
+				x = rs.getString("password");
+				if (x.contentEquals(password)) {
+					System.out.println("[memberRegisterID] Assertion Success.");
+					return true;
+				}
+			}
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
     	}
-    	System.out.println("id inequals password");
+    	System.out.println("[memberRegisterID] Assertion Failed.");
     	return false;
     }
 
-    // id∏¶ ∏≈∞≥∫Øºˆ∑Œ πﬁæ∆ ∏‚πˆ¿« ∏µÁ ¡§∫∏∏¶ class∑Œ π›»Ø
     public Member searchMyInfo(String id) {
     	Member data = new Member();
     	String query = "select * from member where id = ?";
@@ -190,21 +179,22 @@ public class DBCon {
     		pstmt = conn.prepareStatement(query);
     		pstmt.setString(1, id);
     		ResultSet rs = pstmt.executeQuery();
-    		rs.next();
-			data.setID(rs.getString("id"));
-			data.setPassword(rs.getString("password"));
-			data.setGender(rs.getString("gender"));
-			data.setBirth(rs.getString("birth"));
-			data.setNickname(rs.getString("nickname"));
-    		
-    		System.out.println("Calling success22");
+
+    		if(rs.next()){
+				data.setID(rs.getString("id"));
+				data.setPassword(rs.getString("password"));
+				data.setGender(rs.getString("gender"));
+				data.setBirth(rs.getString("birth"));
+				data.setNickname(rs.getString("nickname"));
+				
+				//System.out.println("Calling success22");
+			}
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
     	}
     	return data;
     }
-    // room¿ª ∏≈∞≥∫Øºˆ∑Œ «œø© »∏¿«ø° ¬¸ø©«— id¿ª π›»Ø 
     public ArrayList<String> searchIDByRoom(String room){
     	ArrayList<String> data = new ArrayList<>();
     	String query = "select id from talk where room = ? group by id;";
@@ -217,17 +207,16 @@ public class DBCon {
     		{
     			data.add(new String(rs.getString("id")));
     		}
-    		System.out.println("Calling success");
+    		//System.out.println("Calling success");
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
     	}
     	return data;
     }
-    // room¿ª ∏≈∞≥∫Øºˆ∑Œ «œø© »∏¿«¿« Message¿ª π›»Ø 
     public ArrayList<String> searchMessageByRoom(String room){
     	ArrayList<String> data = new ArrayList<>();
-    	String query = "select 'msg' from talk where room = ?";
+    	String query = "select msg from talk where room = ?";
     	PreparedStatement pstmt = null;
     	try {
     		pstmt = conn.prepareStatement(query);
@@ -237,7 +226,7 @@ public class DBCon {
     		{
     			data.add(new String(rs.getString("msg")));
     		}
-    		System.out.println("Calling success");
+    		//System.out.println("Calling success");
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
@@ -245,7 +234,6 @@ public class DBCon {
     	return data;
     }
     
-    // room¿ª ∏≈∞≥∫Øºˆ∑Œ »∏¿«∞° ¡æ∑·µ»∞Õ¿Œ¡ˆ true-> End, false-> not End or Error?
     public boolean isTalkEnded(String room) {
     	String data;
     	String query = "select * from talk where room= ? order by indexnum desc;";
@@ -254,20 +242,21 @@ public class DBCon {
     		pstmt = conn.prepareStatement(query);
     		pstmt.setString(1, room);
     		ResultSet rs = pstmt.executeQuery();
-    		rs.next();
-    		data = rs.getString("msg");
-    		if (data.compareTo("END")==0) {
-    			return true; // »∏¿«∞° ≥°≥≤
-    		}else {
-    			return false;
-    		}
+    		
+			if(rs.next()){
+				data = rs.getString("msg");
+				if (data.compareTo("END")==0) {
+					return true;
+				}else {
+					return false;
+				}
+			}
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
     	}
     	return false;
     }
-    // input ID, return where participated room 
     public ArrayList<String> searchRoomByID(String id){
     	ArrayList<String> data = new ArrayList<>();
     	String query = "select room from talk where id = ? group by room;";
@@ -280,7 +269,7 @@ public class DBCon {
     		{
     			data.add(new String(rs.getString("room")));
     		}
-    		System.out.println("Calling success");
+    		//System.out.println("Calling success");
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
@@ -296,34 +285,90 @@ public class DBCon {
     		pstmt = conn.prepareStatement(query);
     		pstmt.setString(1, room);
     		ResultSet rs = pstmt.executeQuery();
-    		rs.next();
-    		data = rs.getString("time");
+    		if(rs.next()){
+    			data = rs.getString("time");
+			}
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
     	}
     	return data;
     }
-    
-    public String IdAndRoomForMarked(String id, String room) {
-    	String data = "";
-    	String query = "select marked from mark where id=? and room = ?;";
+
+	public boolean insertMarkData(String id, String number, String marked) {
+    	String query = "insert into mark (id,number,marked) values(?,?,?);";
     	PreparedStatement pstmt = null;
     	try {
     		pstmt = conn.prepareStatement(query);
     		pstmt.setString(1, id);
-    		pstmt.setString(2, room);
-    		ResultSet rs = pstmt.executeQuery();
-    		rs.next();
-    		data = rs.getString("marked");
+    		pstmt.setString(2, number);
+    		pstmt.setString(3, marked);
+    		pstmt.executeUpdate();
+
+
+			return true;
+    		//System.out.println("Insert success");
     	}catch(SQLException e)
     	{
     		e.printStackTrace();
     	}
+
+		return false;
+    }
+
+	public String IdAndRoomForMarked(String id, String room) {
+       String data = "";
+       String query = "select marked from mark where id=? and number = ?;";
+       PreparedStatement pstmt = null;
+       try {
+          pstmt = conn.prepareStatement(query);
+          pstmt.setString(1, id);
+          pstmt.setString(2, room);
+          ResultSet rs = pstmt.executeQuery();
+          if(rs.next()){
+          	data = rs.getString("marked");
+		  }
+       }catch(SQLException e)
+       {
+          e.printStackTrace();
+       }
+       return data;
+    }  
+
+	public List<String> sortByValue(final Map <String , Integer> map){
+        List<String> list = new ArrayList<>();
+        list.addAll(map.keySet());
+         
+        Collections.sort(list,new Comparator(){
+             
+            public int compare(Object o1,Object o2){
+                Object v1 = map.get(o1);
+                Object v2 = map.get(o2);
+                 
+                return ((Comparable) v1).compareTo(v2);
+            }
+             
+        });
+        Collections.reverse(list); // Ï£ºÏÑùÏãú Ïò§Î¶ÑÏ∞®Ïàú
+        return list;
+    }
+
+	public ArrayList<String> extractFiveKeyWordByNLPHashMap(HashMap<String , Integer> tmpMap){
+    	ArrayList<String> data = new ArrayList<>();
+    	Iterator it = sortByValue(tmpMap).iterator();
+    	int i = 0;
+    	while(it.hasNext()){
+    		if (i>5) {
+    			break;
+    		}
+            String temp = (String) it.next();
+            data.add(temp);
+            i++;
+        }
     	return data;
     }
-    
-    public HashMap<String , Integer> NLPHashmapByRoom(String room) {
+
+	public HashMap<String , Integer> NLPHashmapByRoom(String room) {
     	String roomMsg = "";
     	String query = "select msg from talk where room = ?;";
     	PreparedStatement pstmt = null;
@@ -341,7 +386,7 @@ public class DBCon {
     		e.printStackTrace();
     	}
     	// NLP
-    	Komoran komoran = new Komoran("C:\\Users\\fire7\\OneDrive\\πŸ≈¡ »≠∏È\\seokeeDB\\src\\seokee\\models-light");
+    	Komoran komoran = new Komoran("./lib/models-light");
     	List<List<Pair<String,String>>> result = komoran.analyze(roomMsg);
 		HashMap<String , Integer> data = new HashMap <String , Integer>();
 		int tmp;
@@ -359,76 +404,8 @@ public class DBCon {
 		}
     	return data;
     }
-    
-    public static ArrayList<String> extractFiveKeyWordByNLPHashMap(HashMap<String , Integer> tmpMap){
-    	ArrayList<String> data = new ArrayList<>();
-    	Iterator it = sortByValue(tmpMap).iterator();
-    	int i = 0;
-    	while(it.hasNext()){
-    		if (i>5) {
-    			break;
-    		}
-            String temp = (String) it.next();
-            data.add(temp);
-            i++;
-        }
-    	return data;
-    }
-    // extractFiveKeyWordHashmaByRoom ∏¶ Ω««‡«œ±‚ ¿ß«— ∏ﬁº“µÂ
-    public static List<String> sortByValue(final Map <String , Integer> map){
-        List<String> list = new ArrayList<>();
-        list.addAll(map.keySet());
-         
-        Collections.sort(list,new Comparator(){
-             
-            public int compare(Object o1,Object o2){
-                Object v1 = map.get(o1);
-                Object v2 = map.get(o2);
-                 
-                return ((Comparable) v1).compareTo(v2);
-            }
-             
-        });
-        Collections.reverse(list); // ¡÷ºÆΩ√ ø¿∏ß¬˜º¯
-        return list;
-    }
-    
-    
-    // We do not use PYTHON
-    /*
-    public boolean MakeWordCloudByRoom(String room) {
-    	String s = null;
-    	
-    	try {
-    		// print a msg
-    		System.out.println("Executing python code");
-    		String pythonScriptPath = "C:\\Users\\fire7\\eclipse-workspace\\JythonTest\\src\\Seokee\\MakeWordCloudByRoom.py";
-    		String data = room;
-    		String [] cmd = new String [3];
-    		cmd[0] = "python";
-    		cmd[1] = pythonScriptPath;
-    		cmd[2] = data;
-    		Runtime rt = Runtime.getRuntime();
-    		Process process = rt.exec(cmd);
-    		BufferedReader stdInput = new BufferedReader(new
-    				InputStreamReader (process.getInputStream()));
-    		BufferedReader stdError = new BufferedReader(new
-    				InputStreamReader (process.getErrorStream()));
-    		
-    		// read the output from the commend
-    		System.out.println("python should be run");
-    		while ((s = stdInput.readLine())!=null) {
-    			System.out.println(s);
-    		}
-    		while ((s = stdError.readLine())!=null) {
-    			System.out.println(s);
-    		}
-    	}catch(IOException e){
-    		e.printStackTrace();
-    		return false;
-    	}
-    	return true;
-    }*/
+
+}
 
 
 
@@ -484,6 +461,18 @@ class Member {
 	String gender;
 	String birth;
 	String nickname;
+
+	Member(){
+
+	}
+	Member(String id, String pw, String gender, String birth ,String nick ){
+		this.id = id;
+		this.password = pw;
+		this.gender = gender;
+		this.birth = birth;
+		this.nickname = nick;
+	}
+
 	public String getID() {
 		return id;
 	}
@@ -514,5 +503,5 @@ class Member {
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
-}
+
 }
